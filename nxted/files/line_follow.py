5a782a9e9a8c9418bf739e91dff743c9da811d63
mr.Shu <mrshux@gmail.com>
2012-04-27 02:44:15 +0200

    updated line follow

--- nxted/files/line_follow.py
+++ nxted/files/line_follow.py
@@ -5,12 +5,12 @@ def main():
     
     while 1:
         if Sensor(IN_3) < 50:
+            OnRev(OUT_B, 100)
             OnFwd(OUT_A, 100)
-            OnRev(OUT_C, 100)
             
         elif Sensor(IN_1) < 50:
-            OnFwd(OUT_C, 100)
             OnRev(OUT_A, 100)
+            OnFwd(OUT_B, 100)
         
         else:
-            OnFwd(OUT_AC, 80)
+            OnFwd(OUT_AB, 80)
--- nxtemu/__progs__/eline_follow.py
+++ nxtemu/__progs__/eline_follow.py
@@ -7,13 +7,12 @@ def main():
     while 1:
         ticker()#lfixed
         if Sensor(IN_3) < 50:
+            OnRev(OUT_B, 100)
             OnFwd(OUT_A, 100)
-            OnRev(OUT_C, 100)
             
         elif Sensor(IN_1) < 50:
-            OnFwd(OUT_C, 100)
             OnRev(OUT_A, 100)
+            OnFwd(OUT_B, 100)
         
         else:
-            OnFwd(OUT_AC, 80)
-        
+            OnFwd(OUT_AB, 80)
