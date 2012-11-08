385b633c7d557afe0c4b9659b9c78fa9f168804a
Ado Matejov <a.matejov@centrum.sk>
2012-11-08 10:22:58 +0100

    compass example

---
+++ nxted/files/compass.py
@@ -0,0 +1,11 @@
+def main():
+    speed = 0
+    while 1:
+        value = SensorHTCompass(IN_1)
+        
+        if  value > 0 and  value < 180:
+            speed = -100
+        elif value < 360 and  value >= 180:
+                speed = 100
+        OnFwd(OUT_A, speed)
+        OnRev(OUT_B, speed)
---
+++ nxtemu/__progs__/ecompass.py
@@ -0,0 +1,13 @@
+from api import *
+def main():
+    speed = 0
+    while 1:
+        ticker()#lfixed
+        value = SensorHTCompass(IN_1)
+        
+        if  value > 0 and  value < 180:
+            speed = -100
+        elif value < 360 and  value >= 180:
+                speed = 100
+        OnFwd(OUT_A, speed)
+        OnRev(OUT_B, speed)
