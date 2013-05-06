a3afa081c4dc1730cf0160866566234fb9419f80
mr.Shu <mr@shu.io>
2013-05-06 08:56:50 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ setup(name='nxtIDE',
       version='0.7',
       description='the only thing you need to play with Lego robots and Python',
       author='XLC Team',
-      author_email='@',
+      author_email='xlc.team@gmail.com',
       url='http://xlcteam.github.com/nxtIDE',
       packages=['nxtemu', 'nxted'],
+      package_data = {
+          'nxtemu': ['nxtemu/*.yml', 'nxtemu/floor/*'],
+          'nxted' : ['nxted/*.ini', 'nxted/icons/*']
+      },
       entry_points = {
           'console_scripts': [
               'nxtemu = nxtemu.nxtemu:main',
