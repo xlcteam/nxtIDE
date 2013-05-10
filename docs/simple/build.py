bdf71754332787fa1a7a3dd702e72ebecae45bf6
mr.Shu <mr@shu.io>
2013-05-10 23:04:50 +0200

    yaml help path changed

--- docs/simple/build.py
+++ docs/simple/build.py
@@ -97,7 +97,7 @@ def getConstants():
 
 
 
-def exportYaml(fname='../nxted/help.yml', lang='en'):
+def exportYaml(fname='../../nxted/help.yml', lang='en'):
     api = getAPI()
 
     for func,desc in api.iteritems():
