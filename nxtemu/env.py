f711c01a6244b5771e304742f29771bf61c73b86
Ado Matejov <a.matejov@centrum.sk>
2013-12-15 22:00:24 +0100

    fixed checking file sufix

--- nxtemu/env.py
+++ nxtemu/env.py
@@ -22,10 +22,13 @@ screen = pygame.display.get_surface()
 background = pygame.Surface(screen.get_size()).convert()
 
 def write_config(filename = './config.yml'):
-    if not os.path.exists(os.path.dirname(filename)):
-        filename += ".erc"
-    elif not os.path.isfile(filename):
-        filename = os.path.join(filename, "robot.erc")
+    if filename[-4] != ".":
+        # check whether the folder for config file exists
+        if not os.path.exists(os.path.dirname(filename)) or not os.path.exists(filename):
+            filename += ".erc"
+        # if the name of config file is not specified, set it to robot.erc
+        elif not os.path.isfile(filename):
+            filename = os.path.join(filename, "robot.erc")
 
     try:
         stream = open(filename, "w+")
