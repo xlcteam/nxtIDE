fbd20195600ff74da3cf5c3d0e08bc4e11444d07
Ado Matejov <a.matejov@centrum.sk>
2013-12-22 23:01:38 +0100

    added PolyOut function

--- nxtemu/api.py
+++ nxtemu/api.py
@@ -562,6 +562,32 @@ def RectOut(x, y, width, height):
     LineOut(x + width, y, x + width, y - height)
 
 
+def PolyOut(vertices):
+    """
+    .. [en]
+    PolyOut(points)
+
+    Draw a polygon with specified vertices.
+
+    :param 2D array vertices: [[x,y]] coordinates of the vertices.
+
+    .. [/en]
+    .. [sk]
+    PolyOut(vrcholy)
+
+    Vykreslí polygón podľa zadaných vrcholov.
+
+    :param 2D array vertices: [[x,y]] súradnice vrcholov.
+    .. [/sk]
+    """
+
+    for i in range(len(vertices)):
+        if i == len(vertices)-1:
+            LineOut(vertices[i][0], vertices[i][1], vertices[0][0], vertices[0][1])
+        else:
+            LineOut(vertices[i][0], vertices[i][1], vertices[i+1][0], vertices[i+1][1])
+
+
 def ResetScreen():
     """
     .. [en]
